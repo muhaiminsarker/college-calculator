3f23b189ec7518d2580be1c33a93eb6444663cbb
muhaiminsarker <sarker.muhaimin15@gmail.com>
2022-08-02 00:08:06 -0400

    more comments and functions added

--- get_sat_data.py
+++ get_sat_data.py
@@ -10,7 +10,6 @@ import requests
 #This data will be obtained from this website: https://www.4icu.org/us/a-z/
 url = "https://www.4icu.org/us/a-z/"
 
-
 #Open the page
 page = urlopen(url)
 
@@ -20,13 +19,29 @@ html = page.read().decode("utf-8")
 #Create BeautifulSoup object and assign it to a soup variable
 soup = BeautifulSoup(html, "html.parser")
 
-#Find all the a tags (anchor elements) with the href containing "/reviews/" in it
-#These anchor elements will have the names of all the colleges I need 
-university_list = soup.findAll("a", href=re.compile("/reviews/"))
-#Remove final item which is not an college
+soup.find_all("td")
 
-university_list.pop()
-for i in university_list:
-    i.get_text()
+def get_uni_names():
+    """
+    Returns a list with the names of all the college/universities in the US 
+    """
 
+    #Find all the a tags (anchor elements) with the href containing "/reviews/" in it
+    #These anchor elements will have the names of all the colleges I need 
+    uni_tag_list = soup.findAll("a", href=re.compile("/reviews/"))
 
+    #Remove first and final item which is not an college
+    uni_tag_list = uni_tag_list[1:len(uni_tag_list)-1]
+
+    #Obtains the college name rather than the whole anchor element and
+    # puts it into a separate list
+    #List comprehension is used to minimize code length
+    university_list = [i.get_text() for i in uni_tag_list] 
+
+    return university_list
+
+def get_rank():
+    pass
+
+def get_location():
+    pass
